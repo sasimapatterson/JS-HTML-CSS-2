a7bd32615f03f1e65a606baca2a8fd8c99d6896b
Sasi Patterson <67709364+sasimapatterson@users.noreply.github.com>
2021-06-17 19:10:34 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Change the colour of the Instagram icon to #d45bb9
+var instagramIcon = document.querySelector(".fa-instagram");
+instagramIcon.style.color = "#d45bb9";
+
+//Change the colour of the Facebook icon to #6a99b5.
+var facebookIcon = document.querySelector(".fa-facebook");
+facebookIcon.style.color = "#6a99b5";
+
+//Select the opening hours paragraph in the footer. Apply a 1px solid border with a colour of #d45bb9 around the paragraph.
+var openHours = document.querySelector(".contact p");
+openHours.style.color = "#d45bb9";
+openHours.style.border = "solid 1px";
+openHours.style.padding = "1em";
